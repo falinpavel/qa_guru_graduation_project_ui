40924edf123c2833d801ac5fefd59da635362c71
Falin Pavel <falinpavel96@gmail.com>
2025-09-25 00:32:18 +0300

    added docstring

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,10 @@ from helpers.application_manager.application_manager import cm_store
 
 @pytest.fixture(scope="function")
 def full_opening_home_page():
+    """
+    1. Открываем главную страницу
+    2. Проверяем что главная страница открылась
+    3. Принимаем cookies
+    4. Закрываем плашку локации нажимая на крестик
+    """
     return cm_store.home_page.open().is_opened().accept_cookie().close_location_box()
